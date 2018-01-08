d520091564d67e02ea6c6c8215307a1428475912
sibalajp <sibalajp@gmail.com>
2018-01-07 23:19:58 -0800

    initial heroku commit

--- src/containers/dashboard-content/today.js
+++ src/containers/dashboard-content/today.js
@@ -16,6 +16,7 @@ class Today extends Component {
 
 	
 	render () {
+		
 		let myConfig = {
 		  "graphset":[
 		    {	
---
+++ src/tests/week.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { expect, renderComponent } from 'chai';
+import Week from '../containers/dashboard-content/week';
+
+
+describe('Week', () => {
+	it('shoul do something', () => {
+		const wrapper = mount(MainComponent);
+		expect(Week.prototype).to.not.be.null;
+	})
+})
